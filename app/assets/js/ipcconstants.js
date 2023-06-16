cbc719907ee31f813b7cca0cc6309612d90496b0
TenCreator <lucakinsey@gmail.com>
2023-06-16 21:53:04 +0100

    Setup the Microsoft Auth Code

--- app/assets/js/ipcconstants.js
+++ app/assets/js/ipcconstants.js
@@ -1,7 +1,7 @@
 // NOTE FOR THIRD-PARTY
 // REPLACE THIS CLIENT ID WITH YOUR APPLICATION ID.
 // SEE https://github.com/dscalzi/HeliosLauncher/blob/master/docs/MicrosoftAuth.md
-exports.AZURE_CLIENT_ID = '1ce6e35a-126f-48fd-97fb-54d143ac6d45'
+exports.AZURE_CLIENT_ID = '0bb5369a-c6bd-4db7-af24-7bc6a2d817f7'
 // SEE NOTE ABOVE.
 
 
